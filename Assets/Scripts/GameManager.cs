f797fa1d2f5699b1da62c2e7d21368a345248aec
lauravisentin <laura.vise@icloud.com>
2025-09-11 21:59:56 -0400

    Deplacement de la balle (non-teste)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,12 @@ using UnityEngine.SceneManagement; // namespace pour la gestion des scènes
 
 public class GameManager : NetworkBehaviour
 {
-       public static GameManager instance;// Singleton pour parler au GameManager de n'importe où
+    public static GameManager instance;// Singleton pour parler au GameManager de n'importe où
+    
+    public bool partieEnCours;
+    public bool partieTerminee;
 
-   // Création du singleton si nécessaire
-   void Awake()
+    void Awake()
    {
        if (instance == null)
        {
@@ -20,14 +22,39 @@ public class GameManager : NetworkBehaviour
    }
 
    // Fonction appelée pour le bouton qui permet de se connecter comme hôte
-   public void LanceCommeHote() // Public pour être appeler de l'extérieur (par le bouton Hôte)
+   public void LanceCommeHote()
    {
-       NetworkManager.Singleton.StartHost(); // Fonction du NetworkManager pour démarrer une partie comme hôte
+        NetworkManager.Singleton.StartHost();
+   }
+    // Fonction appelée pour le bouton qui permet de se connecter comme client
+    public void LanceCommeClient()
+   {
+        NetworkManager.Singleton.StartClient(); 
    }
 
-   // Fonction appelée pour le bouton qui permet de se connecter comme client
-   public void LanceCommeClient() // Public pour être appeler de l'extérieur (par le bouton Client)
-   {
-       NetworkManager.Singleton.StartClient(); // Fonction du NetworkManager pour démarrer une partie comme client
-   }
+    void Update()
+    {
+        if (!IsHost) return;
+        if (partieEnCours) return;
+
+        if (NetworkManager.Singleton.ConnectedClientsList.Count == 2)
+        {
+            NouvellePartie();
+        }
+    }
+    public void NouvellePartie()
+    {
+        partieEnCours = true;
+    }
+    public void FinPartie()
+    {
+        partieTerminee = true;
+    }
+    public void Recommencer()
+    {
+        NetworkManager.Singleton.Shutdown(); // On arrête le NetworkManager pour réinitialiser la partie
+        Destroy(NetworkManager.gameObject);
+        partieEnCours = false; // On remet la partie en cours à false
+        SceneManager.LoadScene(0);// On recharge la scène de jeu
+    }
 }
