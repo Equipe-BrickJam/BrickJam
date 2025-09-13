c2d0dbc31a24c5ca08cc7f06bb2826ff41eb7d43
Peterson Germain <e2124621@cmaisonneuve.qc.ca>
2025-09-13 16:47:12 -0400

    Avancement de la balle

--- Assets/Scripts/BalleRigid.cs
+++ Assets/Scripts/BalleRigid.cs
@@ -1,16 +1,76 @@
 using UnityEngine;
+using System;
+using System.Collections;
+using Unity.Netcode;
+using TMPro;
+using Unity.Netcode.Components;// pour accéder aux propriétés du NetworkTransform
 
-public class BalleRigid : MonoBehaviour
+public class BalleRigid : NetworkBehaviour
 {
+    public static BalleRigid instance; // Singleton
+    float maxDistanceX = 25f; // moitié de la largeur de la table, pour savoir si un but est compté
+    [SerializeField] private float nombreDeBonds; //compte du nombre de bonds de la balle // Servira plus tard
+    [SerializeField] private float maxSpeed; // si on veut limiter la vitesse max de la balle (inutilisé)
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            return;
+        }
+        Destroy(gameObject);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
+  {
+    if (!IsServer)
     {
-        
+      return;
     }
+    if (!GameManager.instance.partieEnCours) return; // Il faudra créer cette variable dans le GameManager
+
+    //but client
+    if (transform.position.x < -maxDistanceX)
+    {
+      // Ici, il faudra aussi augmenter le score du joueur
+      LanceBalleMilieu();
+    }
+
+    //but serveur (hôte)
+    if (transform.position.x > maxDistanceX)
+    {
+      // Ici, il faudra aussi augmenter le score du joueur
+      LanceBalleMilieu();
+    }
+  }
+
+  public void LanceBalleMilieu()
+    {
+        nombreDeBonds = 0;
+        GetComponent<NetworkTransform>().Interpolate = false;
+        transform.position = new Vector3(0f, 0.5f, 0f);
+        GetComponent<Rigidbody>().linearVelocity = new Vector3(0, 0, 0);
+        if (GameManager.instance.partieTerminee) return; // Il faudra créer cette variable dans le GameManager
+        StartCoroutine(NouvelleBalle());
+    }
+   
+      IEnumerator NouvelleBalle()
+   {
+       yield return new WaitForSecondsRealtime(1f);
+       GetComponent<NetworkTransform>().Interpolate = true;
+
+       System.Random random = new System.Random();
+       float aleaX = random.Next(0, 2) == 0 ? -10 : 10; //  opérateur ternaire
+       float aleaZ = random.Next(0, 2) == 0 ? -10 : 10;
+
+       GetComponent<Rigidbody>().AddForce(aleaX, 0, aleaZ, ForceMode.Impulse);
+   }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,35 @@ using UnityEngine.SceneManagement; // namespace pour la gestion des scènes
 
 public class GameManager : NetworkBehaviour
 {
-       public static GameManager instance;// Singleton pour parler au GameManager de n'importe où
+    public static GameManager instance;// Singleton pour parler au GameManager de n'importe où
+    public bool partieEnCours{ private set; get; } //permet de savoir si une partie est en cours
+    public bool partieTerminee{ private set; get; } // permet de savoir si une partie est terminée
 
-   // Création du singleton si nécessaire
-   void Awake()
+    void Update()
+    {
+        if (!IsHost) return;
+       if (partieEnCours) return;
+
+       if (NetworkManager.Singleton.ConnectedClientsList.Count >= 2)
+       {
+           NouvellePartie();
+       }
+    }
+
+    public void NouvellePartie()
+   {
+       partieEnCours = true;
+       BalleRigid.instance.LanceBalleMilieu();
+   }
+
+    // Fonction appelée par le ScoreManager pour terminer la partie (nous l'utilserons plus tard)
+   public void FinPartie()
+   {
+       partieTerminee = true;
+   }
+
+    // Création du singleton si nécessaire
+    void Awake()
    {
        if (instance == null)
        {
