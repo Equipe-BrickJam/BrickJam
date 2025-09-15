a97d9be61382f6a36df04793220efcefa85649ae
Valérie Giroux-Martone <P1469417@hotmail.com>
2025-09-15 16:29:17 -0400

    OwnerShip
    Ajout de script pour changer l'apparence des perso

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,21 +4,62 @@ using UnityEngine.SceneManagement; // namespace pour la gestion des scènes
 
 public class GameManager : NetworkBehaviour
 {
+    public GameObject Player1;
+    public GameObject Player2;
+
     public static GameManager instance;// Singleton pour parler au GameManager de n'importe où
     public bool partieEnCours{ private set; get; } //permet de savoir si une partie est en cours
     public bool partieTerminee{ private set; get; } // permet de savoir si une partie est terminée
     [SerializeField] private GameObject ballPrefab; // assign prefab in inspector
 
-    void Update()
-    {
-        if (!IsHost) return;
-       if (partieEnCours) return;
 
-       if (NetworkManager.Singleton.ConnectedClientsList.Count >= 2)
+// Création du singleton si nécessaire
+    void Awake()
+   {
+       if (instance == null)
        {
-           NouvellePartie();
+           instance = this;
        }
-    }
+       else
+       {
+           Destroy(gameObject);
+       }
+        // Abonnement au callback OnClientConnectedCallback qui lancera la fonction OnNouveauClientConnecte.
+       NetworkManager.Singleton.OnClientConnectedCallback += OnNouveauClientConnecte;
+   }
+    public void OnNouveauClientConnecte(ulong obj)
+    {
+       if (!IsServer) return;
+
+       if (NetworkManager.Singleton.ConnectedClients.Count == 1) 
+       {
+           GameObject nouveauJoueur = Instantiate(Player1);
+           nouveauJoueur.GetComponent<NetworkObject>().SpawnWithOwnership(obj);
+
+       }
+       else if (NetworkManager.Singleton.ConnectedClients.Count == 2)
+       {
+           GameObject nouveauJoueur = Instantiate(Player2);
+           nouveauJoueur.GetComponent<NetworkObject>().SpawnWithOwnership(obj);
+           
+       }
+       //Mettre une fonction qui commence la partie
+       NouvellePartie();
+       }
+
+    // void Update()
+    // {
+    //     if (!IsHost) return;
+    //    if (partieEnCours) return;
+
+    //    if (NetworkManager.Singleton.ConnectedClientsList.Count >= 2)
+    //    {
+    //        NouvellePartie();
+    //    }
+    // }
+
+    
+   
 
     public void NouvellePartie()
     {
@@ -32,18 +73,7 @@ public class GameManager : NetworkBehaviour
        partieTerminee = true;
    }
 
-    // Création du singleton si nécessaire
-    void Awake()
-   {
-       if (instance == null)
-       {
-           instance = this;
-       }
-       else
-       {
-           Destroy(gameObject);
-       }
-   }
+    
 
    // Fonction appelée pour le bouton qui permet de se connecter comme hôte
    public void LanceCommeHote() // Public pour être appeler de l'extérieur (par le bouton Hôte)
--- Assets/Scripts/Joueur.cs
+++ Assets/Scripts/Joueur.cs
@@ -10,16 +10,13 @@ public class Joueur : NetworkBehaviour
 
     private Vector2 velocitePerso;
 
-    public GameObject Player1;
-    public GameObject Player2;
-
+   
     // Fontion semblale au start, mais pour les objets réseaux et s'éxécute avant
    public override void OnNetworkSpawn()
    {
        base.OnNetworkSpawn();
+      
 
-       // Abonnement au callback OnClientConnectedCallback qui lancera la fonction OnNouveauClientConnecte.
-       NetworkManager.Singleton.OnClientConnectedCallback += OnNouveauClientConnecte;
 
        if (IsServer)
        {
@@ -31,29 +28,6 @@ public class Joueur : NetworkBehaviour
        }
    }
 
-
-    private void OnNouveauClientConnecte(ulong obj)
-   {
-       if (!IsServer) return;
-
-       if (NetworkManager.Singleton.ConnectedClients.Count == 1) 
-       {
-           GameObject nouveauJoueur = Instantiate(Player1);
-           nouveauJoueur.GetComponent<NetworkObject>().SpawnWithOwnership(obj);
-       }
-       else if (NetworkManager.Singleton.ConnectedClients.Count == 2)
-       {
-           GameObject nouveauJoueur = Instantiate(Player2);
-           nouveauJoueur.GetComponent<NetworkObject>().SpawnWithOwnership(obj);
-           
-       }
-       //Mettre une fonction qui commence la partie
-       GetComponent<GameManager>().NouvellePartie();
-   }
-
-
-
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -63,7 +37,7 @@ public class Joueur : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!IsLocalPlayer) return;
+        if(!IsOwner) return;
        
         Mouvement();
     }
