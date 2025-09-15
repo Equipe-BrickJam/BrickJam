0f8848628af7367da8103cc95bef5a1ec77b824d
lauravisentin <laura.vise@icloud.com>
2025-09-15 14:49:11 -0400

    Revert "Deplacement de la balle (non-teste)"
    This reverts commit f797fa1d2f5699b1da62c2e7d21368a345248aec.

--- Assets/Scripts/BalleRigid.cs
+++ Assets/Scripts/BalleRigid.cs
@@ -1,71 +1,16 @@
 using UnityEngine;
-using System.Collections;
-using Unity.Netcode;
-using Unity.Netcode.Components;
-using UnityEngine.UIElements;
-using static UnityEngine.EventSystems.EventTrigger;
-using UnityEngine.SceneManagement;
 
-public class BalleRigid : NetworkBehaviour
+public class BalleRigid : MonoBehaviour
 {
-    public static BalleRigid instance;
-    float maxDistanceX = 5f;
-    private void Awake()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        // Création d'un singleton pour avoir qu'une seule balle.
-        if (instance == null)
-        {
-            instance = this;
-            return;
-        }
-        Destroy(gameObject);
-
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (!IsServer) return;
-
-        if (!GameManager.instance.partieEnCours) return;
-
-        //but client
-        if (transform.position.x < -maxDistanceX)
-        {
-            LanceBalleMilieu();
-        }
-
-        //but serveur (hôte)
-        if (transform.position.x > maxDistanceX)
-        {
-            LanceBalleMilieu();
-        }
-    }
-
-    public void LanceBalleMilieu()
-    {
-        //On désactive l'interpolation du NetworkTransform pour éviter de voir l'interpolation de position de la balle
-        GetComponent<NetworkTransform>().Interpolate = false;
-
-        //On replace la balle au centre de la table et on remet à 0 sa vélocité
-        transform.position = new Vector3(0f, 0.5f, 0f);
-        GetComponent<Rigidbody>().linearVelocity = new Vector3(0, 0, 0);
         
-        if (GameManager.instance.partieTerminee) return;
-
-        //Lancement d'une coroutine qui replacera et relancera la balle seulement si la partie n'est pas terminée
-        StartCoroutine(NouvelleBalle());
     }
-
-    IEnumerator NouvelleBalle()
-    {
-        yield return new WaitForSecondsRealtime(1f);
-        GetComponent<NetworkTransform>().Interpolate = true;
-
-
-        float aleaX = Random.Range(0, 2) == 0 ? -10 : 10; 
-        float aleaZ = Random.Range(0, 2) == 0 ? -10 : 10;
-
-        GetComponent<Rigidbody>().AddForce(aleaX, 0, aleaZ, ForceMode.Impulse);
-    }
-
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,12 +4,10 @@ using UnityEngine.SceneManagement; // namespace pour la gestion des scènes
 
 public class GameManager : NetworkBehaviour
 {
-    public static GameManager instance;// Singleton pour parler au GameManager de n'importe où
-    
-    public bool partieEnCours;
-    public bool partieTerminee;
+       public static GameManager instance;// Singleton pour parler au GameManager de n'importe où
 
-    void Awake()
+   // Création du singleton si nécessaire
+   void Awake()
    {
        if (instance == null)
        {
@@ -22,39 +20,14 @@ public class GameManager : NetworkBehaviour
    }
 
    // Fonction appelée pour le bouton qui permet de se connecter comme hôte
-   public void LanceCommeHote()
+   public void LanceCommeHote() // Public pour être appeler de l'extérieur (par le bouton Hôte)
    {
-        NetworkManager.Singleton.StartHost();
-   }
-    // Fonction appelée pour le bouton qui permet de se connecter comme client
-    public void LanceCommeClient()
-   {
-        NetworkManager.Singleton.StartClient(); 
+       NetworkManager.Singleton.StartHost(); // Fonction du NetworkManager pour démarrer une partie comme hôte
    }
 
-    void Update()
-    {
-        if (!IsHost) return;
-        if (partieEnCours) return;
-
-        if (NetworkManager.Singleton.ConnectedClientsList.Count == 2)
-        {
-            NouvellePartie();
-        }
-    }
-    public void NouvellePartie()
-    {
-        partieEnCours = true;
-    }
-    public void FinPartie()
-    {
-        partieTerminee = true;
-    }
-    public void Recommencer()
-    {
-        NetworkManager.Singleton.Shutdown(); // On arrête le NetworkManager pour réinitialiser la partie
-        Destroy(NetworkManager.gameObject);
-        partieEnCours = false; // On remet la partie en cours à false
-        SceneManager.LoadScene(0);// On recharge la scène de jeu
-    }
+   // Fonction appelée pour le bouton qui permet de se connecter comme client
+   public void LanceCommeClient() // Public pour être appeler de l'extérieur (par le bouton Client)
+   {
+       NetworkManager.Singleton.StartClient(); // Fonction du NetworkManager pour démarrer une partie comme client
+   }
 }
